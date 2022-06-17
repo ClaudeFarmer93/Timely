04587d45731d19ba7a4a9cc50e64215fcfc769bf
Fathonitr <ododmengapa@gmail.com>
2022-06-17 22:54:45 +0200

    Login support first commit

--- controllers/todoListController.js
+++ controllers/todoListController.js
@@ -19,17 +19,25 @@ class TodoListController {
                 res.render("editTodo", { todos: todo, taskId: todoId })
             })
             .catch(error => console.log(error));
-    };
-    static createTodo = (req, res) => {
-        const todoTask = new TodoList({
-            content: req.body.content
-        });
-        todoTask.save()
-            .then(result => console.log(result))
-            .catch(error => console.log(error));
-        res.redirect("/todos/");
-    };
+    };    
+    static createTodo(req, res, next){
+        
+        console.log("############ CALLED ########");
+        const payload = {
+            content: req.body.content,
+            owner: req.body.owner,
+            updated_at: new Date(),
+            created_at: new Date(),
+          };
+        
+        //console.log(payload);
+        TodoList.create(payload)
+        .then(task => console.log(task))
+        .then(console.log("suscess create a task"))
+        .catch(error => console.log(error));
+    }
 
+   
     static updateTodo = (req, res) => {
         const todoId = req.params.id;
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,45 @@
 const User = require("../models/userSchema");
 
-class UserController{
-    static loadRegisterPage(req, res, next){
+class UserController {
+    static loadRegisterPage(req, res, next) {
         res.render("userRegisteration");
     }
-    static register(req, res, next){
+    static loadUserPage(req, res) {
+        const userId = req.params.id
+        res.render("userPage", { userId: userId });
+    }
+    static userLogin(req, res, next) {
+        res.render("login");
+    }
+    static authenticate(req, res, next) {
+        User.findOne({
+            email: req.body.email
+        })
+            .then(user => {
+                if (user && user.password === req.body.password) {
+                    res.redirect(`/users/${user._id}`);
+                    req.flash("success", `${user.fullName}'s logged in successfully!`);
+                    res.user = user;
+                    console.log("test here");
+                    next();
+                } else {
+                    req.flash("error", "Your account or password is incorrect.Please try again or contact your system administrator!");
+                    res.locals.redirect = "/users/login";
+                    next();
+                }
+            })
+            .catch(error => {
+                console.log(`Error logging in user: ${error.message}`);
+                next(error);
+            });
+
         
+    }
+    static redirectView(req, res, next) {
+        console.log("error here");
+    }
+    static register(req, res, next) {
+
         console.log("############ CALLED ########");
         const payload = {
             fullname: req.body.fullname,
@@ -13,14 +47,27 @@ class UserController{
             password: req.body.password,
             updated_at: new Date(),
             created_at: new Date(),
-          };
-        //const findEmail=User.findOne({email: req.body.email});
-        
+        };
+
         console.log(payload);
         User.create(payload)
-        .then(user => console.log(user))
-        .then(console.log("suscess create an user"))
-        .catch(error => console.log(error));
+            .then(user => {
+                req.flash("success", `${user.fullname}'s account created successfully!`);
+                res.redirect("/users");
+                console.log("suscess create an user", res.locals);
+                res.locals.user = user;
+                next();
+            })
+            .catch(error => {
+                console.log(`Error saving user: ${error.message}`);
+                res.redirect("/userRegister");
+                req.flash(
+                    "error",
+                    `Failed to create user account because: ${error.message}.`
+                );
+                next();
+
+            });
     }
     static getAllUser = (req, res) => {
         User.find({})
@@ -34,10 +81,10 @@ class UserController{
     };
     static getUserById = (req, res) => {
         const userId = req.params.id;
-        User.findOne({userId})
+        User.findOne({ userId })
             .exec()
             .then((user) => {
-                console.log("###### User by Id: "+user);
+                console.log("###### User by Id: " + user);
             })
             .catch(error => console.log(error));
     };
@@ -46,7 +93,7 @@ class UserController{
         const userId = req.params.id;
 
         User.findByIdAndUpdate(userId, { content: req.body.content })
-           .then(console.log("###### User Updated ######"))
+            .then(console.log("###### User Updated ######"))
             .catch(error => console.log(error));
     };
 
