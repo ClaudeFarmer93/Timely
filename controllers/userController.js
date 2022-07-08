eadbe67f141aa612a621e6d06fe3e36ba1d33580
Fathonitr <ododmengapa@gmail.com>
2022-07-08 22:17:42 +0200

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,63 @@
 const User = require("../models/userSchema");
-
+const token = process.env.TOKEN || "todoT0k3n";
 class UserController {
+
+    static verifyToken = (req, res, next) => {
+        let token = req.query.apiToken;
+        if (token) {
+            User.findOne({ apiToken: token })
+                .then(user => {
+                    if (user) next();
+                    else next(new Error("Invalid API token."));
+                })
+                .catch(error => {
+                    next(new Error(error.message));
+                });
+        } else {
+            next(new Error("Invalid API token."));
+        }
+    }
     static loadRegisterPage(req, res, next) {
         res.render("userRegisteration");
     }
     static loadUserPage(req, res) {
         const userId = req.params.id
-        res.render("userPage", { userId: userId });
+        User.findOne({ userId })
+            .exec()
+            .then((user) => {
+                console.log("###### User by Id: " + typeof (user));
+                console.log(user.fullname);
+                res.render("userPage", { userId: user });
+            })
+            .catch(error => console.log(error));
     }
     static userLogin(req, res, next) {
         res.render("login");
     }
     static authenticate(req, res, next) {
-        User.findOne({
-            email: req.body.email
-        })
+        console.log("##### LOGIN ATTEMP ####");
+        User.findOne({ email: req.body.email })
             .then(user => {
-                if (user && user.password === req.body.password) {
-                    res.redirect(`/users/${user._id}`);
-                    req.flash("success", `${user.fullName}'s logged in successfully!`);
-                    res.user = user;
-                    console.log("test here");
-                    next();
+                if (user) {
+                    user.passwordComparison(req.body.password)
+                        .then(passwordsMatch => {
+                            if (passwordsMatch) {
+                                console.log(user);
+                                res.redirect = `/users/${user._id}`;
+                                //req.flash("success", `${user.fullName}'s logged in successfully!`);
+                                console.log("success", `${user.fullName}'s logged in successfully!`);
+                                res.user = user;
+                            } else {
+                                //req.flash("error", "Failed to log in user account: Incorrect Password.");
+                                console.log("error", "Failed to log in user account: Incorrect Password.");
+                                res.redirect = "/users/login";
+                            }
+                            next();
+                        });
                 } else {
-                    req.flash("error", "Your account or password is incorrect.Please try again or contact your system administrator!");
-                    res.locals.redirect = "/users/login";
+                    //req.flash("error", "Failed to log in user account: User account not found.");
+                    console.log("error", "Failed to log in user account: User account not found.");
+                    res.redirect = "/users/login";
                     next();
                 }
             })
@@ -32,11 +65,9 @@ class UserController {
                 console.log(`Error logging in user: ${error.message}`);
                 next(error);
             });
-
-        
     }
     static redirectView(req, res, next) {
-        console.log("error here");
+        console.log("error here redirect");
     }
     static register(req, res, next) {
 
@@ -52,10 +83,10 @@ class UserController {
         console.log(payload);
         User.create(payload)
             .then(user => {
-                req.flash("success", `${user.fullname}'s account created successfully!`);
+                //req.flash("success", `${user.fullname}'s account created successfully!`);
                 res.redirect("/users");
                 console.log("suscess create an user", res.locals);
-                res.locals.user = user;
+                //res.locals.user = user;
                 next();
             })
             .catch(error => {
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     fullname: {
@@ -21,4 +22,25 @@ const userSchema = mongoose.Schema({
         type: Date,
     }
 });
+userSchema.pre("save", function (next) {
+    let user = this;
+    if (!user.apiToken) user.apiToken = randToken.generate(16);
+    next();
+});
+userSchema.pre("save", function (next) {
+    let user = this;
+    bcrypt.hash(user.password, 10).then(hash => {
+        user.password = hash;
+        next();
+    })
+        .catch(error => {
+            console.log(`Error in hashing password: ${error.message}`);
+            next(error);
+        });
+});
+userSchema.methods.passwordComparison = function (inputPassword) {
+    let user = this;
+    return bcrypt.compare(inputPassword, user.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
